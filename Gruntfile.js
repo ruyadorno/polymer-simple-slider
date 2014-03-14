35ecd4e5bb807e6481eba1408911c2750872e5a1
Ruy Adorno <ruyadorno@hotmail.com>
2014-03-14 00:56:54 -0400

    Added grunt-bump plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,25 @@
 'use strict';
 
 module.exports = function (grunt) {
+
+  grunt.loadNpmTasks('grunt-bump');
+
+  grunt.initConfig({
+    bump: {
+      options: {
+        files: ['package.json', 'bower.json', 'README.md'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['-a'], // '-a' for all files
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'origin master',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
+      }
+    }
+  });
+
 };
